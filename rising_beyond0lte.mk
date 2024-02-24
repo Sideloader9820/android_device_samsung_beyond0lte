073fd9f873d2b9cfeb30ec5feb9ace4d31fff660
Sideloader9820 <160022449+Sideloader9820@users.noreply.github.com>
2024-02-24 12:41:07 +0000

    rising_beyond0lte: adapt to rising

--- rising_beyond0lte.mk
+++ rising_beyond0lte.mk
@@ -31,13 +31,13 @@ TARGET_SCREEN_HEIGHT := 2280
 TARGET_SCREEN_WIDTH := 1080
 
 ## Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/rising/config/rising.mk)
 
 ## Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 ## Device identifier, this must come after all inclusions
-PRODUCT_NAME := lineage_beyond0lte
+PRODUCT_NAME := rising_beyond0lte
 PRODUCT_DEVICE := beyond0lte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G970F
